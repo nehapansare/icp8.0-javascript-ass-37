1e339cf2fb839c309898eda434814db9f0f2ab87
nehapansare <nehapansare007@gmail.com>
2024-08-03 19:06:49 +0530

    localstorage concept added

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ const tasks = [
     "Go to the market",
     "Do Study",
 ];
+function localStorageTask() {
+    const loadTask = JSON.parse(localStorage.getItem('localTask'));
+    if (loadTask) {
+        tasks.unshift(...loadTask);
+    }
+    loadTaskTasks();  
+}
+localStorageTask();
+
+function loadTaskTasks() {
+    localStorage.setItem('localTask', JSON.stringify(tasks));
+    const taskContainer = document.getElementById("task-container");
+    taskContainer.innerHTML = '';
+
+    for (const task of tasks) {
+        taskContainer.innerHTML += `
+        <div class="todo-item">
+            ${task}
+            <p class="btn-del" onclick="del('${task}')"><img src="delete.png" width="20px"></p>
+        </div>`;
+    }
+}
+
